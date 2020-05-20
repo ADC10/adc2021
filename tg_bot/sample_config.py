473ce87217a3aa4a4f8956beb633401081ff37af
ADC10 <65671115+ADC10@users.noreply.github.com>
2020-05-20 21:49:59 +0200

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = ""
-    OWNER_ID = "683538773" # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "ViruZs"
+    API_KEY = "1110453723:AAH2tM1XecAuASu83GcRd0d-A4g1BMfqlNc"
+    OWNER_ID = "1013112355" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "kingpipo18"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -23,17 +23,17 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
+    SUDO_USERS = [254318997 683538773 570400686 466337795]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [254318997 683538773 570400686 466337795]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [254318997 683538773 570400686 466337795]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    DONATION_LINK = https://www.paypal.me/kingpipo18  # EG, paypal
     CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
+    PORT = 8443
+    DEL_CMDS = True  # Whether or not you should delete "blue text must click" commands
+    STRICT_GBAN = True
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
+    ALLOW_EXCL = True  # Allow ! commands as well as /
 
 
 class Production(Config):
